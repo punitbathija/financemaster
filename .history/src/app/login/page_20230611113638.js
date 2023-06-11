ccb8954fa2d18fad5c85c30d140f0a39b3459414
Punit Bathija <punit644@gmail.com>
2023-06-11 11:37:30 +0530

    Init a login screen

--- .history/src/app/login/page_20230611113638.js
+++ .history/src/app/login/page_20230611113638.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div className="text-6xl text-center p-4">OM NAMO SHIVAY</div>;
+  return <div>page</div>;
 };
 
 export default page;
--- .history/src/app/page_20230609121155.js
+++ .history/src/app/page_20230609121155.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div className="text-6xl text-center p-4">OM NAMO SHIVAY</div>;
+  return <div className="text-6xl text-center p-4">Hello World!</div>;
 };
 
 export default page;
